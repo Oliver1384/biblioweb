2210f9aae6380baf71e9e98bdd00139b3537a44b
Oliver1384 <oliver_artiles@hotmail.com>
2022-02-18 11:42:52 +0000

    each user can see his  loans

--- resources/views/user/userProfile.blade.php
+++ resources/views/user/userProfile.blade.php
@@ -59,7 +59,7 @@
         </tr>
         @foreach ($booksLoan as $bookLoan)
             @foreach($books as $book)
-                @if($book->id === $bookLoan->book_loan_id)
+                @if($book->id === $bookLoan->book_loan_id && @Auth::user()->id === $bookLoan->user_loan_id)
                     <tr>
                         <td><img src="{{ $book['image'] }}" width="100px" alt="portada del libro"></td>
                         <td>{{ $book['name']}}</td>
