0bc5ad42572a2558de320c604d40790a4862f281
Oliver1384 <oliver_artiles@hotmail.com>
2022-02-18 11:32:29 +0000

    users can loan a books and remove from the enable list

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -18,22 +18,25 @@ class LoanController extends Controller {
 
 
     public function store(Request $request) {
-
-        $request->validate([
-            'book_loan_id' => 'required|min:1|max:1000',
-            'user_loan_id' => 'required|min:1|max:1000',
-        ]);
-        $input = $request->all();
-
+        $loans = Loan::query()
+            ->orWhere('loans.book_loan_id', '=',  $request->all()["book_loan_id"])
+            ->select('loans.id')
+            ->get();
+        if (count($loans) === 0){
+            $request->validate([
+                'book_loan_id' => 'required|min:1|max:1000',
+                'user_loan_id' => 'required|min:1|max:1000',
+            ]);
+            $input = $request->all();
+            Loan::create($input);
+        }
         /*if ($image = $request->file('image')) {
             $imageDestinationPath = 'uploads/';
             $postImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
             $image->move($imageDestinationPath, $postImage);
             $input['image'] = "$postImage";
         }*/
-
-        Loan::create($input);
-        return redirect()->route('userProfile')->with('success','Tendrás disponible el libro durante 14 días');
+        return redirect()->route('userProfile');
     }
 
 
--- resources/views/user/userProfile.blade.php
+++ resources/views/user/userProfile.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <h1>Desde vista de usuario</h1>
+    @if(isset($error))
+        <p>{{$error}}</p>
+    @endif
     <table class="table table-bordered">
         <tr>
             <th>Imagen</th>
@@ -11,27 +14,40 @@
             <th>Categoria</th>
         </tr>
         @foreach ($books as $book)
-            <tr>
-                <td><img src="{{ $book['image'] }}" width="100px" alt="portada del libro"></td>
-                <td>{{ $book['name']}}</td>
-                <td>{{$book['author']}}</td>
-                <td>{{$book['editorial']}}</td>
-                <td>{{$book['category']}}</td>
-                <td class="text-center">
-                    <form class="solicitar" method="post" action="{{route('loans.store',['book_loan_id'=> $book->id,'user_loan_id'=>@Auth::user()->id])}}">
-                        @csrf
-                        @error('loan')
-                        <span class="invalid-feedback" role="alert">
+            @php
+                $loan = true;
+            @endphp
+
+            @foreach($booksLoan as $bookLoan)
+                @if($book->id === $bookLoan->book_loan_id)
+                    @php
+                        $loan = false;
+                    @endphp
+                @endif
+            @endforeach
+
+            @if($loan)
+                <tr>
+                    <td><img src="{{ $book['image'] }}" width="100px" alt="portada del libro"></td>
+                    <td>{{ $book['name']}}</td>
+                    <td>{{$book['author']}}</td>
+                    <td>{{$book['editorial']}}</td>
+                    <td>{{$book['category']}}</td>
+                    <td class="text-center">
+                        <form class="solicitar" method="post" action="{{route('loans.store',['book_loan_id'=> $book->id,'user_loan_id'=>@Auth::user()->id])}}">
+                            @csrf
+                            @error('loan')
+                            <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                        @enderror
-                        <button type="submit">solicitar</button>
-                    </form>
-                </td>
-            </tr>
+                            @enderror
+                            <button type="submit">solicitar</button>
+                        </form>
+                    </td>
+                </tr>
+            @endif
         @endforeach
     </table>
-
     <table class="table table-bordered">
         <h1>Libros en prestamo</h1>
         <tr>
@@ -50,17 +66,6 @@
                         <td>{{$book['author']}}</td>
                         <td>{{$book['editorial']}}</td>
                         <td>{{$book['category']}}</td>
-                        <td class="text-center">
-                            <form class="solicitar" method="post" action="{{route('loans.store',['book_loan_id'=> $book->id,'user_loan_id'=>@Auth::user()->id])}}">
-                                @csrf
-                                @error('loan')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                                <button type="submit">solicitar</button>
-                            </form>
-                        </td>
                     </tr>
                 @endif
             @endforeach
