2e1285c6595fec5771d776aa5c295b1dcb1d4665
oliver <oliver_artiles@hotmail.com>
2022-02-20 16:27:14 +0000

    user can only loan two books

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -4,31 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Loan;
 use App\Models\RequestLoan;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
-class LoanController extends Controller
-{
-
+class LoanController extends Controller {
 
     public function store(Request $request) {
         $loans = Loan::query()
             ->orWhere('loans.book_loan_id', '=', $request->all()["book_loan_id"])
             ->select('loans.id')
             ->get();
-        if (count($loans) === 0) {
+        $userId = $request->query()["user_loan_id"];
+        $currentUserloans = Loan::query()
+            ->orWhere('loans.user_loan_id', '=', $userId)
+            ->select('loans.id')
+            ->get();
+        if (count($loans) === 0 && count($currentUserloans) < 2) {
             $request->validate([
                 'book_loan_id' => 'required|min:1|max:1000',
                 'user_loan_id' => 'required|min:1|max:1000',
             ]);
             $input = $request->all();
             Loan::create($input);
-        }
-        $requestsLoan = RequestLoan::query()
-            ->orWhere('request_loans.book_loan_id', '=', $request->all()["book_loan_id"])
-            ->select('request_loans.id')
-            ->get();
-        foreach($requestsLoan as $requestLoan) {
-            (new RequestLoanController)->destroy($requestLoan);
+            $requestsLoan = RequestLoan::query()
+                ->orWhere('request_loans.book_loan_id', '=', $request->all()["book_loan_id"])
+                ->select('request_loans.id')
+                ->get();
+            foreach($requestsLoan as $requestLoan) {
+                (new RequestLoanController)->destroy($requestLoan);
+            }
         }
         return redirect()->route('manageLoans');
     }
--- app/Http/Controllers/RequestLoanController.php
+++ app/Http/Controllers/RequestLoanController.php
@@ -2,19 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Loan;
 use App\Models\RequestLoan;
 use Illuminate\Http\Request;
 
 class RequestLoanController extends Controller
 {
-    public function store(Request $request) {
-        $request->validate([
-            'book_loan_id' => 'required|min:1|max:1000',
-            'user_loan_id' => 'required|min:1|max:1000',
-        ]);
-        $input = $request->all();
-        RequestLoan::create($input);
+    protected function store(Request $request) {
+        $userId = $request->query()["user_loan_id"];
 
+        $loans = Loan::query()
+            ->orWhere('loans.user_loan_id', '=', $userId)
+            ->select('loans.id')
+            ->get();
+        if(count($loans) < 2){
+            $request->validate([
+                'book_loan_id' => 'required|min:1|max:1000',
+                'user_loan_id' => 'required|min:1|max:1000',
+            ]);
+            $input = $request->all();
+            RequestLoan::create($input);
+        }
         return redirect()->route('userProfile');
     }
 
@@ -22,4 +30,6 @@ class RequestLoanController extends Controller
         $requestLoan->delete();
         return redirect()->route('manageLoans');
     }
+
+
 }
